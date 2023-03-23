c8f710847a8a2170b9c00804eaeb6b59dd8f34f6
christellevs <christelle.marie@gmail.com>
2023-03-23 17:56:07 +0000

    test: add a couple more tests

--- src/test/specs/http.test.ts
+++ src/test/specs/http.test.ts
@@ -8,7 +8,25 @@ describe('Http Service Runner', () => {
         runtime.setModule('HelloService');
         const res = await fetch(runtime.baseUrl + '?name=World');
         const text = await res.text();
+        
+        assert.strictEqual(res.status, 200)
         assert.strictEqual(text, 'Hello, World');
+    });
+    
+    it('returns undefined text from Hello Service when param name is incorrect', async () => {
+        runtime.setModule('HelloService');
+        const res = await fetch(runtime.baseUrl + '?foo=World');
+        const text = await res.text();
+        
+        assert.strictEqual(res.status, 200)
+        assert.strictEqual(text, 'Hello, undefined');
+    });
+    
+    it('returns 500 when module URL is incorrect', async () => {
+        runtime.setModule('NotFoundModule');
+        const res = await fetch(runtime.baseUrl + '?name=World');
+        
+        assert.strictEqual(res.status, 500);
     });
 
 });
